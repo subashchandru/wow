05bee92dc1a03afbdf4a2b6cc2cf56738c75e5d8
subashchandru <subhashchandru@outlook.com>
2019-12-22 16:10:43 +0530

    Create Jansi.java

---
+++ lib/src/main/java/com/example/lib/Jansi.java
@@ -0,0 +1,13 @@
+package com.example.lib;
+
+public class Jansi
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	public String dei(){
+	    
+	    return "iruken iruken"
+	}
+}
